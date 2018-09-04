98535cef50f6910205bd164861bc1f9517782c49
Gerard <gerard.marti@upf.edu>
2018-09-04 16:56:35 +0200

    Added io functions. Added crop_volume in utils.py Started to initialize a base configuration.

--- dataio.py
+++ dataio.py
@@ -1,5 +1,124 @@
 """
 Data in-out.
 
-FUnction that deal with input and output of data and conversion to tensors.
+FUnctions that deal with input and output of data and conversion to tensors.
+
+Most of the data in/out funcionality is gathered from library DLTK:
+https://github.com/DLTK
 """
+import SimpleITK as sitk
+import os
+import tensorflow as tf
+import pandas as pd
+import time
+import numpy as np
+from matplotlib import pyplot as plt
+from dltk.io.augmentation import *
+from dltk.io.preprocessing import *
+from keras.utils import Sequence
+
+from utils import crop_volume
+
+class BrainSequence(Sequence):
+    """
+    Implement the Generator to train the model.
+
+    Based on keras.utils.Sequence()
+    """
+
+    def __init__(self, x_set, y_set, batch_size, **kwargs):
+        """
+        Initialize class.
+
+        x_set: list of paths to the images.
+        y_set: associated classes.
+        batch_size: Size of the training batch.
+        n_slices: number of slices to get from each image.
+        args: parameters for data augmentation
+        x_set and y_set should have same length
+        """
+        self.x, self.y = x_set, y_set
+        self.batch_size = batch_size
+        self.kwargs = kwargs
+
+    def __len__(self):
+        """Return length of the steps per epoch."""
+        return int(np.ceil(len(self.x) / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        """
+        Return a full batch of data for training.
+
+        This procedure must also do data augmentation steps.
+        idx: internal parameter
+        """
+        # Get indices of each image
+        batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        batch_img = []
+        for file in batch_x:
+            img = load_img(file)
+
+            # Data preparation
+
+            # Substract the mean of the trained set from the base images bla bla
+            # TODO
+            # Duplicate the image in the three channels.
+            # # TODO:
+            # Data augmentation:
+            if self.args["Gaussian noise"]:
+                # Apply gaussian noise with a given probability
+                print(1)
+            if self.args["Flipped"]:
+                # Flip images with a given probability
+                print(2)
+
+            batch_img.append(img)
+        return np.array(batch_img), np.array(batch_y)
+
+
+def slice_generator(image_files, n_slices, size, out_dir):
+    """
+    Image generator.
+
+    creates slices of a given size over a dataset of images and saves them
+    to disk.
+    image_filenames: list of image files in disk.
+    n_slices: number of slices for each image.
+    size: size of the slices.
+    out_dir: output directory.
+    """
+    # for each image file
+    for path in image_files:
+        # load img
+        img = load_img(path)
+        # For each slice
+        for n in range(n_slices):
+            # Crop the image
+            img_crop = crop_volume(img, size, mode="Random")
+            # Create output dir
+            out_file = out_dir + os.path.basename(path) + '_crop' + str(n) + '.png'
+            save_img(img_crop, out_file)
+
+
+def save_img(img, path):
+    """
+    Save a given image to the path specified.
+
+    img: image to save.
+    path: path where to save the image.
+    """
+    img_out = sitk.GetImageFromArray(img)
+    sitk.WriteImage(img_out, path)
+
+
+def load_img(path):
+    """
+    Load a single image from disk.
+
+    path: Image path
+    """
+    sitk_t1 = sitk.ReadImage(path)
+    img = sitk.GetArrayFromImage(sitk_t1)
+    return img
--- utils.py
+++ utils.py
@@ -3,3 +3,33 @@ Utils.
 
 Other functions that are useful for the implementation.
 """
+
+import random
+
+
+def crop_volume(img, size, mode="Random"):
+    """
+    Crop an slice of a fixed size from an image.
+
+    img: image to crop.
+    size: size of the cropping:
+    mode: Type of cropping. Modes supported:
+        Random: random cropping, that fits inside the image.
+    """
+    if mode == "Random":
+        # size of img
+        x, y, z = img.shape
+        x_crop, y_crop, z_crop = size
+        # the initial point can go from 0 to x - x_crop.
+        new_x = random.randint(0, x-x_crop-1)
+        new_y = random.randint(0, y-y_crop-1)
+        new_z = random.randint(0, z-z_crop-1)
+
+        img_cropped = img[new_x:new_x+x_crop,
+                          new_y:new_y+y_crop,
+                          new_z:new_z+z_crop]
+
+        return img_cropped
+
+    else:
+        raise ValueError("Mode of cropping not supported!")
